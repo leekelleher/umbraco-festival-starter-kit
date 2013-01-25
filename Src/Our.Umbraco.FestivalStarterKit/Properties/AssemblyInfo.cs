8ef0bc9b797fc93970821e00afd9a0b98980098c
leekelleher <leekelleher@gmail.com>
2013-01-25 16:54:05 +0000

    Added website files; MVC Views.

---
+++ Src/Our.Umbraco.FestivalStarterKit/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Our.Umbraco.FestivalStarterKit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Our.Umbraco.FestivalStarterKit")]
+[assembly: AssemblyCopyright("Copyright \xa9 Umbrella Inc Ltd 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("d78b0d05-8812-431c-9f4b-98cd7a473c94")]
