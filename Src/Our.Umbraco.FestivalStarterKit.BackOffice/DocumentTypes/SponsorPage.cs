ede7608e81b7a7322fc6d656b3c4fca1d95a1cbd
leekelleher <leekelleher@gmail.com>
2013-02-19 21:49:10 +0000

    Amends to the Speakers and Sponsor listing templates.

--- Src/Our.Umbraco.FestivalStarterKit.BackOffice/DocumentTypes/SponsorPage.cs
+++ Src/Our.Umbraco.FestivalStarterKit.BackOffice/DocumentTypes/SponsorPage.cs
@@ -9,10 +9,21 @@ namespace Our.Umbraco.FestivalStarterKit.BackOffice.DocumentTypes
 	[DocumentType(Name = "SponsorPage", IconUrl = "folder.gif",
 		Alias = "SponsorPage",
 		AllowedTemplates = new string[] { "SponsorPage" },
-		AllowedChildNodeTypes = new Type[] { })]
+		AllowedChildNodeTypes = new Type[] { },
+		Mixins = new Type[] { typeof(Mixins.Content), typeof(Mixins.Meta), typeof(Mixins.UmbracoProperties) })]
 	public partial class SponsorPage : FestivalStarterKit
 	{
+		#region ctor
+
 		public SponsorPage() { }
 		public SponsorPage(int nodeId) : base(nodeId) { }
+
+		#endregion
+
+		[DocumentTypeProperty(UmbracoPropertyType.Upload, Name = "Sponsor Logo", Description = "", Tab = Tabs.Content, Mandatory = true)]
+		public string SponsorLogo { get; set; }
+
+		[DocumentTypeProperty(UmbracoPropertyType.Textstring, Name = "Sponsor Link", Description = "The URL to the sponsor's website.", Tab = Tabs.Content, Mandatory = true)]
+		public string SponsorLink { get; set; }
 	}
 }
